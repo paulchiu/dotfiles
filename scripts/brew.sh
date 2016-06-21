db0814d0dd3000bfd1b27af66e129b86ea8e466c
Paul Chiu <paulchiu@gmail.com>
2016-06-21 16:13:29 +1000

    Updated brew to include lnav.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -9,6 +9,7 @@ brew install tmux  \
 && brew install tree \
 && brew install nvm \
 && brew install wget \
+&& brew install lnav \
 && brew install homebrew/versions/bash-completion2 \
 && brew tap homebrew/completions \
 && brew install z
