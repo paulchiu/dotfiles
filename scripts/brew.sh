9665df693ce8d36d0f3c223928899c7f5d194e2b
Paul Chiu <paulchiu@gmail.com>
2016-06-21 10:58:33 +1000

    Added more brew default installs.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -e /usr/local/bin/brew]
+if [ ! -e /usr/local/bin/brew ]
 then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -9,4 +9,6 @@ brew install tmux  \
 && brew install tree \
 && brew install nvm \
 && brew install wget \
+&& brew install homebrew/versions/bash-completion2 \
+&& brew tap homebrew/completions \
 && brew install z
