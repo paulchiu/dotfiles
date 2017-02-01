631baca3a782ef67e04d07a9e86968dfba3eea14
Paul Chiu <paulchiu@gmail.com>
2017-02-02 07:00:05 +1000

    Refactored brew install command.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -5,16 +5,17 @@ then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install tmux  \
-&& brew install tree \
-&& brew install nvm \
-&& brew install wget \
-&& brew install lnav \
-&& brew install homebrew/versions/bash-completion2 \
-&& brew tap homebrew/completions \
-&& brew install z \
-&& brew install ag \
-&& brew install Caskroom/cask/osxfuse \
-&& brew install homebrew/fuse/sshfs \
-&& brew install yarn \
-&& brew install editorconfig
+brew install \
+    tmux \
+    tree \
+    nvm \
+    wget \
+    lnav \
+    homebrew/versions/bash-completion2 \
+    z \
+    ag \
+    Caskroom/cask/osxfuse \
+    homebrew/fuse/sshfs \
+    yarn \
+    editorconfig
+&& brew tap homebrew/completions
