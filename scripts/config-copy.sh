c889e9fd1868a202340f7e1e5b7fd7d494607b0a
Paul Chiu <paulchiu@gmail.com>
2017-02-06 06:44:49 +1000

    Made config copy and vimrc more platform aware.

--- scripts/config-copy.sh
+++ scripts/config-copy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DEST=~/
+PLATFORM=$(uname)
 
 # Copy config files
 cp bash/.bash_profile ${DEST}
@@ -8,3 +9,9 @@ cp bash/.bash_prompt ${DEST}
 cp bash/.aliases ${DEST}
 cp git/.gitconfig ${DEST}
 cp tmux/.tmux.conf ${DEST}
+
+# Make platform specific changes
+if [[ ${PLATFORM} == "Linux" && -e /usr/bin/apt ]]; then
+    mv ${DEST}.bashrc ${DEST}.bashrc-original
+    mv ${DEST}.bash_profile ${DEST}.bashrc
+fi
