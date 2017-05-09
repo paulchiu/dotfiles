56c0212357cccb5d67a1c4ac11fce054de7c8241
Joe Refoy <hello@joerefoy.com>
2017-05-09 10:13:21 +1000

    * Ensure tmux plugins directory is writable * Reload tmux

--- scripts/tpm.sh
+++ scripts/tpm.sh
@@ -5,7 +5,13 @@ DEST=~/
 # Install tmux plugin manager manually if not exist
 if [[ ! -e "${DEST}/.tmux/plugins/tpm/tpm" ]]; then
     git clone https://github.com/tmux-plugins/tpm "${DEST}/.tmux/plugins/tpm"
+
+    # Make plugins directory writable
+    chmod -R 777 ~/.tmux/plugins
 fi
 
 # Trigger plugin installation
 "${DEST}/.tmux/plugins/tpm/bin/install_plugins"
+
+# Reload TMUX environment so TPM is sourced
+tmux source ~/.tmux.conf
