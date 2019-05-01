a13a78f9ed58bae5697161d736f31e2d924d7374
Paul Chiu <paulchiu@gmail.com>
2019-05-01 10:15:56 +1000

    Added better font install script to dnf gui script.

--- scripts/dnf-gui.sh
+++ scripts/dnf-gui.sh
@@ -8,3 +8,8 @@ sudo dnf update \
   gpaste \
   gnome-shell-extension-gpaste \
   gnome-tweaks
+
+# Install better fonts (to be tested)
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf copr enable dawid/better_fonts
+dnf install fontconfig-enhanced-defaults fontconfig-font-replacements
