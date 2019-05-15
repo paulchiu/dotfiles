5e9fd920c215fa0bfd69180ff5cd562165e82eeb
paulchiu <paulchiu@gmail.com>
2019-05-15 11:55:24 +1000

    Update/fedora fonts (#5)
    * Added better font install script to dnf gui script.
    * Added neofetch.

--- scripts/dnf-gui.sh
+++ scripts/dnf-gui.sh
@@ -8,3 +8,8 @@ sudo dnf update \
   gpaste \
   gnome-shell-extension-gpaste \
   gnome-tweaks
+
+# Install better fonts (to be tested)
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf copr enable dawid/better_fonts
+sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements
