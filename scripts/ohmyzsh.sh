70a5f4d63610a14ff78a55736ecabfc049fdbc27
Paul Chiu <paulchiu@gmail.com>
2020-03-23 12:44:20 +1000

    Fixed ohmyzsh script, added preferred iterm2 colour scheme

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone https://github.com/dijitalmunky/nvm-auto.git "$ZSH_CUSTOM/plugins/nvm-auto"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
+ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
+git clone https://github.com/dijitalmunky/nvm-auto.git "$ZSH/plugins/nvm-auto"
