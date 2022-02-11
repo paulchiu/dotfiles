8594175d1d4699fcb27a23ad8a3f5fd2e3edc621
paulchiu <paulchiu@gmail.com>
2022-02-12 07:38:28 +1000

    feat(brew): Added more MacOS GUI apps

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,28 +1,44 @@
 #!/bin/sh
 
-if [ ! -e /usr/local/bin/brew ]
+if [ ! -e /opt/homebrew/bin/brew ]
 then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew install \
+    dbeaver-community \
+    dropbox \
+    ag \
+    bat \
+    discord \
+    editorconfig \
+    easy-move-plus-resize \
+    fd \
+    firefox \
+    gitui \
+    google-drive \
+    hiddenbar \
+    iterm2 \
+    karabiner-elements \
+    keycastr \
+    lnav \
+    ngrok \
+    nvm \
+    maccy \
+    macvim \
+    messenger \
+    p7zip \
+    postman \
+    ranger \
+    raycast \
+    rectangle \
+    slack \
+    stats \
     tmux \
     tree \
-    nvm \
+    visual-studio-code \
     wget \
-    lnav \
-    z \
-    ag \
     yarn \
-    editorconfig \
-    p7zip \
-    ranger \
-    fd \
-    bat \
-    gitui \
-    keycastr \
-    stats \
-    raycast \
-    ngrok \
-    hidenbar
-
+    z \
+    zappy \
+    zoom
