ff6ce16dc04905d421d5d813d9340676ef97aac2
Paul Chiu <paulchiu@gmail.com>
2026-05-02 21:20:54 +1000

    chore(shell): remove shadow backup script
    Drop the old Shadow database backup helper now that it has been deleted locally.

--- bin/shadow_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Backup shadow.db to local backups folder
-BACKUP_DIR="$HOME/Downloads/Resources/Shadow"
-mkdir -p "$BACKUP_DIR"
-
-# Create timestamped backup
-cp "/Users/paul/Library/Application Support/com.taperlabs.shadow/shadow.db" "$BACKUP_DIR/shadow.db" 2>&1 | while read line; do echo "$(date '+%Y-%m-%d %H:%M:%S') - $line"; done
-date +"%Y-%m-%d %H:%M:%S - Backup completed" >>"$BACKUP_DIR/backup.log"
